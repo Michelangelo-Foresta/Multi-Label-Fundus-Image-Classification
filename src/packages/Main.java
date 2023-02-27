d8b857caf0dec1ab874fab68d6f8efc8ad8b7d04
Thomas-Ng-dev <tng.github@gmail.com>
2023-02-26 22:19:17 -0500

    Added resource bundle for languages.

--- src/packages/Main.java
+++ src/packages/Main.java
@@ -1,12 +1,16 @@
 package packages;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, IOException {
         // Assemble all the pieces of MVC
-
+    	Locale locale = new Locale("en-CA", "fr-CA");
+    	ResourceBundle resourceBundle = ResourceBundle.getBundle("ResourceBundle", locale);
+    	resourceBundle.getString("Languages");
         PatientView view = new PatientView("Eye Disease Diagnostic System");
         PatientController pController = new PatientController(view);
         pController.retrieveAllData();
